a5bb832699f5fd42612e060859b74201971bfd1d
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-06-19 18:33:49 -0500

    Se modifica section de skills

--- Portafolio/src/Skills.jsx
+++ Portafolio/src/Skills.jsx
@@ -22,7 +22,10 @@ export function Skills() {
                 <h2 className="skills__title">Habilidades</h2>
                 <figure className="skills__figure">
                     {uniqueSkills.map((skill, index) => (
-                        <img src={`/Icons/${skill.src}`} alt={skill.alt} className='skills__img' key={index} />
+                        <div key={index} className='skills__item'>
+                        <img src={`/Icons/${skill.src}`} alt={skill.alt} className='skills__img' />
+                        <span className='skills__name'>{skill.alt.replace(' Logo', '')}</span>
+                    </div>
                     ))}
 
                 </figure>

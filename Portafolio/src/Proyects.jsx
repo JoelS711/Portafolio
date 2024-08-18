444d97264a4c69cd62477ef5c724c1c71f5eb0b5
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-08-18 13:26:04 -0500

    Update Proyects.jsx

--- Portafolio/src/Proyects.jsx
+++ Portafolio/src/Proyects.jsx
@@ -1,43 +1,71 @@
-import './styles/proyects.css';
-import  dataProyect  from './assets/dataProyect.json';
-
+import "./styles/proyects.css";
+import dataProyect_es from "./assets/dataProyect_es.json";
 
 export function Proyects() {
-
-    return (
-        <>
-            <section className="separator">
-                <div className="separator__line"></div>
-            </section>
-            <section className='proyects' id='proyects'>
-                <h2 className="proyects__title">Proyectos</h2>
-                <p className="proyects__description">He trabajo en proyectos utilizando Java, JavaScript, Spring Boot, HTML, CSS, Angular, React y Python, entre otros. Mi enfoque abarca desde aplicaciones web hasta backend, trabajando con bases de datos relacionales y no relacionales para garantizar la calidad y eficiencia en cada proyecto.</p>
-                <article className='proyects__cards'>
-                    {dataProyect.map((proyect, index) => (
-                        <div className="proyects__card" key={index}>
-                            <div className="proyects__image">
-                                <figure className="proyects__figure">
-                                    <img src={`/Proyects/${proyect.image}`} alt={proyect.nameProyect} className="proyects__figure--img" />
-                                </figure>
-                            </div>
-                            <h3 className="proyects__name">{proyect.nameProyect}</h3>
-                            <div className='proyects__icons'>
-                                <figure className='proyects__icon'>
-                                    {proyect.icons.map((icon, indexIcon) => (
-                                        <img src={`/Icons/${icon.src}`} alt={icon.alt} className={icon.className} key={indexIcon} />
-                                    ))}
-                                </figure>
-                            </div>
-                            <p className="proyects__description--p">{proyect.description}</p>
-                            <div className="proyects__buttons">
-                                <button className="proyects__button"><a href={proyect.github} target="_blank" className='proyects__button--link'>Github</a></button>
-                                <button className="proyects__button"><a href={proyect.preview} target="_blank" className='proyects__button--link' >Preview</a></button>
-                            </div>
-
-                        </div>
-                    ))}
-                </article>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="separator">
+        <div className="separator__line"></div>
+      </section>
+      <section className="proyects" id="proyects">
+        <h2 className="proyects__title">Proyectos</h2>
+        <p className="proyects__description">
+          He trabajo en proyectos utilizando Java, JavaScript, Spring Boot,
+          HTML, CSS, Angular, React y Python, entre otros. Mi enfoque abarca
+          desde aplicaciones web hasta backend, trabajando con bases de datos
+          relacionales y no relacionales para garantizar la calidad y eficiencia
+          en cada proyecto.
+        </p>
+        <article className="proyects__cards">
+          {dataProyect_es.map((proyect, index) => (
+            <div className="proyects__card" key={index}>
+              <div className="proyects__image">
+                <figure className="proyects__figure">
+                  <img
+                    src={`/Proyects/${proyect.image}`}
+                    alt={proyect.nameProyect}
+                    className="proyects__figure--img"
+                  />
+                </figure>
+              </div>
+              <h3 className="proyects__name">{proyect.nameProyect}</h3>
+              <div className="proyects__icons">
+                <figure className="proyects__icon">
+                  {proyect.icons.map((icon, indexIcon) => (
+                    <img
+                      src={`/Icons/${icon.src}`}
+                      alt={icon.alt}
+                      className={icon.className}
+                      key={indexIcon}
+                    />
+                  ))}
+                </figure>
+              </div>
+              <p className="proyects__description--p">{proyect.description}</p>
+              <div className="proyects__buttons">
+                <button className="proyects__button">
+                  <a
+                    href={proyect.github}
+                    target="_blank"
+                    className="proyects__button--link"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="proyects__button">
+                  <a
+                    href={proyect.preview}
+                    target="_blank"
+                    className="proyects__button--link"
+                  >
+                    Preview
+                  </a>
+                </button>
+              </div>
+            </div>
+          ))}
+        </article>
+      </section>
+    </>
+  );
 }

253aa405862bcc51d9468ff5e87baf5c61dd0efb
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-08-21 21:00:18 -0500

    About.jsx is modified

--- Portafolio/src/About.jsx
+++ Portafolio/src/About.jsx
@@ -1,24 +1,36 @@
-import './styles/about.css';
+import "./styles/about.css";
 
-export function About() {
-    return (
-        <section className='about' id='about'>
-            <h2 className='about__title'>Acerca de mi</h2>
-            <p className='about__p'>Soy un apasionado desarrollador de software con experiencia en tecnologías modernas. Me encanta trabajar en proyectos desafiantes y colaborar con equipos creativos para ofrecer soluciones innovadoras. Mi enfoque se centra en crear experiencias digitales impactantes y de alta calidad que mejoren la vida de las personas.</p>
-            <div className='about__cards'>
-                <figure className='about__cards--card'>
-                    <img src='/unomas.png' alt="Mas de un anio" className='about__cards--img' />
-                    <p>Soy un desarrollador con más de un año de experiencia en mis propios proyectos, enfocado en la innovación y la resolución creativa de problemas.</p>
-                </figure>
-                <figure className='about__cards--card'>
-                <img src='/graduation-hat2.png' alt="Graduation" className='about__cards--img'/>
-                    <p>Egresado como tecnologo en sistemas de la Universidad ECCI y actualmente estudiante de Ingenieria de Software</p>
-                </figure>
-                <figure className='about__cards--card'>
-                <img src='/diploma.png' alt="Graduation" className='about__cards--img'/>
-                    <p>Diplomado en desarrollo de software Universidad del Bosque y MinTic</p>
-                </figure>
-            </div>
-        </section>
-    )
+export function About({ content }) {
+  return (
+    <section className="about" id="about">
+      <h2 className="about__title">{content.sections.about_me.title}</h2>
+      <p className="about__p">{content.sections.about_me.description}</p>
+      <div className="about__cards">
+        <figure className="about__cards--card">
+          <img
+            src="/unomas.png"
+            alt="Mas de un anio"
+            className="about__cards--img"
+          />
+          <p>{content.sections.about_me.additionalInfo[1]}</p>
+        </figure>
+        <figure className="about__cards--card">
+          <img
+            src="/graduation-hat2.png"
+            alt="Graduation"
+            className="about__cards--img"
+          />
+          <p>{content.sections.about_me.additionalInfo[2]}</p>
+        </figure>
+        <figure className="about__cards--card">
+          <img
+            src="/diploma.png"
+            alt="Graduation"
+            className="about__cards--img"
+          />
+          <p>{content.sections.about_me.additionalInfo[3]}</p>
+        </figure>
+      </div>
+    </section>
+  );
 }

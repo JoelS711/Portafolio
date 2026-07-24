f2147c81f7af34542ea245f6db9bc18790bb4453
JoelS711 <joelsanabria11@gmail.com>
2026-07-24 19:05:56 -0300

    Add reveal and staggered entrance animations to experience cards.

--- Portafolio/src/components/Experience.tsx
+++ Portafolio/src/components/Experience.tsx
@@ -1,19 +1,27 @@
 import "@/styles/experience.css";
 import { ComponentProps } from "../types/common";
+import { useReveal } from "../hooks/useReveal";
 
 export function Experience({ content }: ComponentProps) {
+  const sectionRef = useReveal<HTMLElement>();
+  const cardsRef = useReveal<HTMLDivElement>(true);
+
   return (
     <>
       <section className="separator">
         <div className="separator__line"></div>
       </section>
-      <section className="experience" id="experience">
+      <section className="experience" id="experience" ref={sectionRef}>
         <h2 className="experience__title">
           {content.sections.experience.title}
         </h2>
-        <div className="experience__cards">
+        <div className="experience__cards" ref={cardsRef}>
           {content.sections.experience.jobs.map((job, index) => (
-            <div key={index} className="experience__card">
+            <div
+              key={index}
+              className="experience__card"
+              style={{ "--stagger-index": index } as React.CSSProperties}
+            >
               <div className="experience__card--titlecontain">
                 <h4 className="experience__card--title">{job.date}</h4>
               </div>
